257691b14488ba21db594f861f21d2c7125dff27
Anderson Lu <gzluxueyan@gmail.com>
2018-10-22 04:36:55 +0800

    add test

---
+++ application/application_test.go
@@ -0,0 +1,27 @@
+package app_helper
+
+import (
+	"testing"
+)
+
+func BenchmarkTypeConvert1(b *testing.B) {
+	var d interface{} = 1
+	var total = 0
+	b.StartTimer()
+	for i := 0; i < 1000000; i++ {
+		total += d.(int)
+	}
+
+	b.StopTimer()
+}
+
+func BenchmarkTypeConvert2(b *testing.B) {
+	var d int = 1
+	var total = 0
+	b.StartTimer()
+	for i := 0; i < 1000000; i++ {
+		total += d
+	}
+
+	b.StopTimer()
+}
