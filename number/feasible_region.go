43d4e611c83be825f2c9c126a0d159bf10a3d305
luxueyan <luxueyan@truxing.com>
2018-10-22 18:27:45 +0800

    add 一阶差分

---
+++ number/feasible_region.go
@@ -0,0 +1,10 @@
+package number_helper
+
+//计算一阶差分
+func CalcFeasibleRegion(x []float64) []float64 {
+	y := make([]float64, len(x)-1)
+	for i := 0; i < len(x)-1; i++ {
+		y[i] = x[i+1] - x[i]
+	}
+	return y
+}
