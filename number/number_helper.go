a6380f37e290f6a0638e9ab25869d89e4d3aa06a
luxueyan <luxueyan@truxing.com>
2018-10-22 19:23:12 +0800

    add Ln(x)

--- number/number_helper.go
+++ number/number_helper.go
@@ -153,3 +153,29 @@ func Round(f float64, n int) float64 {
 	}
 	return FloorOrCeil(count+n, f, false)
 }
+
+func f(x, a float64) float64 {
+	return math.Exp(x) - a
+}
+
+func Ln(n float64) float64 {
+	var lo, hi, m float64
+	if n <= 0 {
+		return -1
+	}
+	if n == 1 {
+		return 0
+	}
+	EPS := 0.00001
+	lo = 0
+	hi = n
+	for math.Abs(lo-hi) >= EPS {
+		m = float64((lo + hi) / 2.0)
+		if f(m, n) < 0 {
+			lo = m
+		} else {
+			hi = m
+		}
+	}
+	return float64((lo + hi) / 2.0)
+}
