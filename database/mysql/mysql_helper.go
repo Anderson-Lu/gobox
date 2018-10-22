286d107ba7ac9291e414f89280a082a7cb35dceb
luxueyan <luxueyan@truxing.com>
2018-10-22 16:40:53 +0800

    modify mysql

--- database/mysql/mysql_helper.go
+++ database/mysql/mysql_helper.go
@@ -1,14 +1,13 @@
 package mysql_helper
 
-import(
+import (
+	"fmt"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
-) 
+)
 
-func NewMysqlClient(host, dbname, user, pwd string) (gorm.DB*, error) {	
+func NewMysqlClient(host, dbname, user, pwd string) (*gorm.DB, error) {
 	db, err := gorm.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local", user, pwd, host, dbname))
-	return &CRUD{
-		client:      db,
-		redisHelper: redisHelper,
-	}, err
-} 
+	return db, err
+}
