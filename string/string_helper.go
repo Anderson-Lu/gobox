4a8668625cf89acda776d78516106db25ee1ccd5
Anderson Lu <306887931@qq.com>
2019-04-11 14:27:45 +0800

    Update string_helper.go

--- string/string_helper.go
+++ string/string_helper.go
@@ -3,6 +3,7 @@ package string_helper
 import "regexp"
 
 var urlRegexp *regexp.Regexp
+var src = rand.NewSource(time.Now().UnixNano())
 
 func init() {
 	urlRegexp = regexp.MustCompile(`(https?|ftp|file)://[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]`)
@@ -13,4 +14,25 @@ func init() {
  */
 func FindUrl(raw string) []string {
 	return urlRegexp.FindAllString(raw, -1)
+}
+
+/*
+* Generate a random string
+*/
+func RandStringBytesMaskImprSrc(n int) string {
+    b := make([]byte, n)
+    // A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
+    for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+        if remain == 0 {
+            cache, remain = src.Int63(), letterIdxMax
+        }
+        if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+            b[i] = letterBytes[idx]
+            i--
+        }
+        cache >>= letterIdxBits
+        remain--
+    }
+
+    return string(b)
 }
